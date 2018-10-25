66924fa8f01beca5072b6a501b8bcb898d51c016
koba <daisuke-kobayashi@cybozu.co.jp>
2018-10-25 18:50:00 +0900

    モバイルのURLを生成する部分を関数に切り出した

--- dist/container.js
+++ dist/container.js
@@ -1,11 +1,16 @@
 (function() {
-    const url = location.origin + '/k/m/';
+
+    function getMobileUrl(location) {
+        return location.origin + '/k/m/'
+    }
+
+    const mobileUrl = getMobileUrl(location);
     const previewEl = document.createElement('a');
     previewEl.className = 'device-preview';
-    previewEl.setAttribute('href', url);
+    previewEl.setAttribute('href', mobileUrl);
     previewEl.addEventListener('click', function(event) {
         event.preventDefault();
-        window.open(url, '_blank', 
+        window.open(mobileUrl, '_blank', 
         'width=375,height=667,top=10,left=10');
     });
     const linksEl = document.querySelector('.gaia-header-toolbar-links');
