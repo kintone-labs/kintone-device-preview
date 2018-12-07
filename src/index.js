52b656e49e069dfbe599f7a82b004b9f7e1d250a
koba <daisuke-kobayashi@cybozu.co.jp>
2018-12-07 09:54:09 +0900

    converterが必要なくなったので削除

--- src/index.js
+++ src/index.js
@@ -1,31 +1,4 @@
-import ReportUrlConverter from './urlconverter/reporturlconverter';
-import RecordAddUrlConverter from './urlconverter/recordaddurlconverter';
-import RecordDetailUrlConverter from './urlconverter/recorddetailurlconverter';
-import RecordEditUrlConverter from './urlconverter/recordediturlconverter';
-import RecordIndexUrlConverter from './urlconverter/recordindexurlconverter';
-import RecordReuseUrlConverter from './urlconverter/recordreuseurlconverter';
-import SearchUrlConverter from './urlconverter/searchurlconverter';
-import SpaceIndexUrlConverter from './urlconverter/spaceindexurlconverter';
-
 (function() {
-
-    function getMobileUrl(location) {
-        const converters = [
-            new ReportUrlConverter(),
-            new RecordEditUrlConverter(),
-            new RecordReuseUrlConverter(),
-            new RecordAddUrlConverter(),
-            new RecordDetailUrlConverter(),
-            new RecordIndexUrlConverter(),
-            new SearchUrlConverter(),
-            new SpaceIndexUrlConverter()
-        ];
-        const converter = converters.find(converter => converter.canSupportUrl(location.href));
-        if (converter) {
-            return converter.convertToMobileUrl(location);
-        }
-        return location.origin + '/k/m/';
-    }
 
     function createPreviewLink() {
         const previewEl = document.createElement('a');
