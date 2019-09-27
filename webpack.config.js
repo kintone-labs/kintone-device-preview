8fdb7b22d8d01dfbe4f9a01a333c8eed3bcd624b
sukoyakarizumu <sukoyakarizumu@gmail.com>
2019-09-27 10:56:22 +0900

    add 'build' command

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,41 @@
-const path = require('path');
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: {
-        'js/index': './src/js/index.js',
-        'js/bg': './src/js/bg.js',
-        'css/style': './src/css/style.css'
-    },
-    output: {
-        path: path.join(__dirname, 'dist')
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        }),
-        new CopyWebpackPlugin([{
-            from: 'src/img',
-            to: 'img'
-        }]),
-        new CopyWebpackPlugin([{
-            from: 'src/manifest.json',
-            to: 'manifest.json'
-        }]),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader'
-                ]
-            }
-        ]
-    }
-}
+  mode: "none",
+  entry: {
+    "js/index": "./src/js/index.js",
+    "js/bg": "./src/js/bg.js",
+    "css/style": "./src/css/style.css"
+  },
+  output: {
+    path: path.join(__dirname, "dist")
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: "src/img",
+        to: "img"
+      }
+    ]),
+    new CopyWebpackPlugin([
+      {
+        from: "src/manifest.json",
+        to: "manifest.json"
+      }
+    ])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  }
+};
